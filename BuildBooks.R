180f123f0dde223396b2f24936b978ee9e6042a7
j-steinhart <jsth@e.mail.de>
2022-08-02 12:14:04 +0200

    Few Updates

---
+++ BuildBooks.R
@@ -0,0 +1,6 @@
+bookdown::render_book(
+    input = "index.Rmd",
+    output_format = "bookdown::pdf_document2")
+bookdown::render_book(
+    input = "index.Rmd",
+    output_format = "bookdown::gitbook")
